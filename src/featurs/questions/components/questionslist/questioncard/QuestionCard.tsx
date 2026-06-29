781c310ca6127c7e90dc3ad8e31e2ab6cc4fb3c9
Mostafa Kamari <Mostafamf555@gmail.com>
2026-06-30 02:19:18 +0330

    fix(question-list-responsive): resolve styling issue

--- src/featurs/questions/components/questionslist/questioncard/QuestionCard.tsx
+++ src/featurs/questions/components/questionslist/questioncard/QuestionCard.tsx
@@ -13,7 +13,7 @@ type QuestionCardProps = {
 
 const QuestionCard = ({ question, author, tags, created_at }: Omit<QuestionCardProps, 'id'>) => {
   return (
-    <article className="w-full relative flex lg:flex-row flex-col gap-x-6 min-h-40 shrink-0 rounded-xl bg-secondary border border-foreground/20 p-4">
+    <article className="w-full relative flex lg:flex-row flex-col gap-x-6 min-h-40 shrink-0 rounded-xl bg-secondary border border-foreground/20 p-4 max-lg:pt-10">
       <AnswerCount />
       <Separator className="hidden lg:block" orientation="vertical" />
       <div className="flex flex-col justify-between flex-1 lg:gap-y-0 gap-y-4 min-w-0">
